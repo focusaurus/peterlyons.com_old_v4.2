c92fdb3a39159143c36f0753a27b613a2d8d4c68
plyons <plyons@b4a2011e-84f8-0310-afcc-9cede453a3b6>
2002-10-27 16:10:53 +0000

    Initial revision
    git-svn-id: file:///var/opt/svnroot/trunk/peterlyons.com@133 b4a2011e-84f8-0310-afcc-9cede453a3b6

---
+++ photogallery.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+print "Content-type: text/html\r\n\r\n";
+print `cat ./header.part`;
+$docroot = "/opt/www/peterlyons.com";
+$galleryDir = "photos"; 
+if( $ENV{"QUERY_STRING"} =~ /gallery=([^&]+)/ ) {
+    $gallery = $1;
+}
+if( $ENV{"QUERY_STRING"} =~ /picture=([^&]+)/ ) {
+    $picture = $1;
+}
+@galleries = <$docroot/$galleryDir/*>;
+$uri = $ENV{"REQUEST_URI"};
+$length = index($uri, ".pl")+3;
+$uri = substr($uri,0,$length);
+if ( ! $gallery ) {
+    $gallery = "portugal";
+}
+foreach (<$docroot/$galleryDir/$gallery/*.*>) {
+    $_ = substr( $_, rindex($_, "/")+1);
+    if ( /^(.*).alt.txt$/ ) {
+	$altTxts{$1} = $_;
+	next;
+    }
+    if ( /^(.*)-TN.jpg$/ ) {
+	$thumbnails{$1} = $_;
+	next;
+    }
+    if ( /^(.*).jpg$/ ) {
+	$pictures{$1} = $_;
+	next;
+    }
+}
+if (! -e "$docroot/$galleryDir/$gallery/$picture.jpg" ) {
+    $picture = (keys %pictures)[0];
+}
+$pictAlt = `cat $docroot/$galleryDir/$gallery/$altTxts{$picture}`;
+chomp $pictAlt;
+print "\n<table><tr><td valign=\"top\">\n";
+print "<img alt=\"$pictAlt\" src=\"/$galleryDir/$gallery/$picture.jpg\" />";
+print "</td><td valign=\"top\">Galleries:<br /><br />";
+foreach ( @galleries ) {
+    $dir = substr( $_, rindex($_, "/")+1);
+    print "<a href=\"$uri?gallery=$dir\">$dir</a><br /><br />";
+}
+print "</td></tr></table>";
+
+print "<h1 class=\"caption\">$pictAlt</h1>\n";
+
+foreach (keys %thumbnails) {
+    $thumbAlt = `cat $docroot/$galleryDir/$gallery/$altTxts{$_}`;
+    chomp $thumbAlt;
+    print "<a href=\"$uri?gallery=$gallery&picture=$_\">";
+    print "<img border=\"0\" src=\"/$galleryDir/$gallery/$_-TN.jpg\" alt=\"$thumbAlt\"></a> \n";
+}
+print `cat ./footer.part`;
+
+
