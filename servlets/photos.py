eefaafcd3c1b13e5feb79d8187807c2303fc2645
plyons <plyons@b4a2011e-84f8-0310-afcc-9cede453a3b6>
2006-07-23 15:46:42 +0000

    Added the ability to read captions from IPTC IIM metadata inside jpg images and fall back to separate .alt.txt file
    git-svn-id: file:///var/opt/svnroot/trunk/peterlyons.com@479 b4a2011e-84f8-0310-afcc-9cede453a3b6

--- servlets/photos.py
+++ servlets/photos.py
@@ -17,27 +17,27 @@ class photos(baseservlet):
         self.gallery = trans.request().field('gallery', defaultGallery)
         photoName = trans.request().field('photo', None)
         self.galleries = os.listdir(photosDir)
-	self.galleries.sort()
+        self.galleries.sort()
         if self.gallery not in self.galleries:
             print 'WARNING: invalid gallery in request: "%s"' % self.gallery
             self.gallery = defaultGallery
         self.galleryURI = photosURI + '/' + self.gallery
         self.galleryDir = os.path.join(photosDir, self.gallery)
         #this includes full paths to both full size and thumbnail image files
-        thumbnailPaths= glob.glob(os.path.join(self.galleryDir, '*-TN.jpg'))
+        thumbnailPaths= glob.glob(os.path.join(self.galleryDir, '*' + Photo.thumbnailExtension))
         self.photos = []
         self.photo = None
         for path in thumbnailPaths:
             photo = Photo()
             photo.name = os.path.split(path)[1][:-7]
-            photo.thumbnailURI = self.galleryURI + '/' + photo.name + '-TN.jpg'
-            photo.fullSizeURI = self.galleryURI + '/' + photo.name + '.jpg'
+            photo.thumbnailURI = self.galleryURI + '/' + photo.name + Photo.thumbnailExtension
+            photo.fullSizeURI = self.galleryURI + '/' + photo.name + Photo.fileExtension
             photo.caption = self.caption(photo.name)
             self.photos.append(photo)
             if photoName == photo.name:
                 #this is the current photo to show full size
                 self.photo = photo
-   
+
         if self.photo is None:
             self.photo = self.photos[0]
             if photoName is not None:
@@ -46,7 +46,13 @@ class photos(baseservlet):
     def caption(self, photoName):
         altTxt = ''
         try:
-            altFile = file(os.path.join(self.galleryDir, photoName + '.alt.txt'))
+            from iptcinfo import IPTCInfo
+            altTxt = IPTCInfo(os.path.join(self.galleryDir, photoName + Photo.fileExtension)).data[120]
+        except:
+            #No problem.  Fall back to .alt.txt method
+            pass
+        try:
+            altFile = file(os.path.join(self.galleryDir, photoName + Photo.captionExtension))
             altTxt = altFile.read()
             altFile.close()
         except IOError:
@@ -54,7 +60,10 @@ class photos(baseservlet):
         return string.strip(altTxt)
 
 class Photo(object):
-    
+    fileExtension = '.jpg'
+    thumbnailExtension = '-TN.jpg'
+    captionExtension = '.alt.txt'
+
     def __init__(self):
         self.name = None
         self.fullSizeURI = None
