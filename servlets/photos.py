0041108a3914ca5fc67972030751200a44826855
Peter Lyons <plyons@dext.localdomain>
2008-02-03 19:42:57 -0700

    Park City photo update. The drive from Boulder.

--- servlets/photos.py
+++ servlets/photos.py
@@ -8,7 +8,7 @@ import sys
 #edit these values to configure for different environments
 photosURI = '/photos'
 photosDir = '/var/www/peterlyons.com/photos'
-defaultGallery = 'boulder_2007'
+defaultGallery = 'park_city_2008'
 
 class photos(baseservlet):
 
