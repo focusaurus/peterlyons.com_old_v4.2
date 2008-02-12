98f32527c72524fcd669cd29c9e3b8c14956f4a3
Peter Lyons <plyons@dext.localdomain>
2008-02-11 20:32:18 -0700

    exclude php.html from rsync

--- bin/stage_to_production.sh
+++ bin/stage_to_production.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . `dirname ${0}`/site_conf.sh
-rsync -e ssh -az "${STATIC}/" "${HOST}:${STATIC}"
+rsync -e ssh -az --exclude php.html "${STATIC}/" "${HOST}:${STATIC}"
 rsync -e ssh -az  --exclude ErrorMsgs --exclude Cache --exclude Logs --exclude appserverpid.txt "${APP}/" "${HOST}:${APP}"
