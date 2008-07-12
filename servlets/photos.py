d6a4bf1e632b2c6d0610b11345716d5ad7b7ce25
Peter Lyons <plyons@dext.localdomain>
2008-07-12 11:19:57 -0700

    Big photo update

--- servlets/photos.py
+++ servlets/photos.py
@@ -8,7 +8,7 @@ import sys
 #edit these values to configure for different environments
 photosURI = "/photos"
 photosDir = "/var/www/peterlyons.com/photos"
-defaultGallery = "portland_2008_part_1"
+defaultGallery = "jacksonville_2008"
 
 class photos(baseservlet):
 
