1264ac65e14dedf7f809fb9c952bef7290b0768c
Peter Lyons <plyons@dext.localdomain>
2008-07-12 21:29:05 -0700

    Second round of flagstaff photos

--- servlets/photos.py
+++ servlets/photos.py
@@ -8,7 +8,7 @@ import sys
 #edit these values to configure for different environments
 photosURI = "/photos"
 photosDir = "/var/www/peterlyons.com/photos"
-defaultGallery = "jacksonville_2008"
+defaultGallery = "flagstaff_2008_part_1"
 
 class photos(baseservlet):
 
