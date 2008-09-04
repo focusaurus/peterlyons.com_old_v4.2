e8e123761705a6fe3a1a27e5e546dbcd2079973f
Peter Lyons <plyons@dext.localdomain>
2008-09-03 21:31:29 -0600

    Burning Man 2008 photos

--- servlets/photos.py
+++ servlets/photos.py
@@ -8,7 +8,7 @@ import sys
 #edit these values to configure for different environments
 photosURI = "/photos"
 photosDir = "/var/www/peterlyons.com/photos"
-defaultGallery = "flagstaff_2008_part_2"
+defaultGallery = "burning_man_2008"
 
 class photos(baseservlet):
 
