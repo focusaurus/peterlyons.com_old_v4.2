32b46cc29d9a31bdac743e6c1ac9695ee9118f61
Peter Lyons <pete@peterlyons.com>
2010-09-23 21:16:30 -0600

    Updated to minimal <!doctype html>

--- bin/stage_to_production.sh
+++ bin/stage_to_production.sh
@@ -6,4 +6,3 @@ rsync -aiEz${1} "--exclude-from=${WORK}/bin/exclude_app.txt" "${APP}/" "${HOST}:
 rsync -iEz${1} /etc/init.d/webware_peterlyons.com /etc/init.d/spawn-fcgi_peterlyons.com "${HOST}:/etc/init.d"
 rsync -iEz${1} /etc/init.d/webware_peterlyons.com /etc/init.d/spawn-fcgi_peterlyons.com "${HOST}:/etc/init.d"
 rsync -iEz${1} "${WORK}/overlay/etc/nginx/sites-available/peterlyons.com" "${HOST}:/etc/nginx/sites-available"
-rsync -iEz${1} "${WORK}/etc/nginx/sites-enabled/001-peterlyons.com" "${HOST}:/etc/nginx/sites-enabled"
--- overlay/var/www/webware/peterlyons.com/peterlyons.com/photos.py
+++ overlay/var/www/webware/peterlyons.com/peterlyons.com/photos.py
@@ -8,7 +8,7 @@ import sys
 #edit these values to configure for different environments
 photosURI = "/photos"
 photosDir = "/var/www/peterlyons.com/photos"
-defaultGallery = "summer_2010"
+defaultGallery = "fall_2010"
 
 class photos(baseservlet):
 
