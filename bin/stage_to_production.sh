5d9b5fb22db926d72121c15168ec0adbdfa6af85
Peter Lyons <pete@peterlyons.com>
2011-01-02 12:17:09 -0700

    Updates from the VM migration from hank (slicehost) to hubb (linode)

--- bin/stage_to_production.sh
+++ bin/stage_to_production.sh
@@ -4,5 +4,4 @@
 rsync -aiEz${1} "--exclude-from=${WORK}/bin/exclude_static.txt" "${STATIC}/" "${HOST}:${STATIC}"
 rsync -aiEz${1} "--exclude-from=${WORK}/bin/exclude_app.txt" "${APP}/" "${HOST}:${APP}"
 rsync -iEz${1} /etc/init.d/webware_peterlyons.com /etc/init.d/spawn-fcgi_peterlyons.com "${HOST}:/etc/init.d"
-rsync -iEz${1} /etc/init.d/webware_peterlyons.com /etc/init.d/spawn-fcgi_peterlyons.com "${HOST}:/etc/init.d"
 rsync -iEz${1} "${WORK}/overlay/etc/nginx/sites-available/peterlyons.com" "${HOST}:/etc/nginx/sites-available"
