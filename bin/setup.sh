f223f243f6502d09e6ed84857cbc2fa63d634b6c
Peter Lyons <pete@peterlyons.com>
2011-01-08 05:52:37 -0700

    Added monit configuration to monitor webware for peterlyons.com photo gallery

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,4 +9,8 @@ link() {
 }
 link "/etc/monit/conf.d/spawn-fcgi_${SITE}.monitrc"
 link "/etc/monit/conf.d/nginx_${SITE}.monitrc"
+link "/etc/monit/conf.d/webware_${SITE}.monitrc"
+#link "/etc/mysql/my.cnf"
+#link "/etc/mysql/debian-start"
+#link "/etc/init/mysql.conf"
 cp "${OVERLAY}/etc/monit/monitrc" /etc/monit/monitrc
