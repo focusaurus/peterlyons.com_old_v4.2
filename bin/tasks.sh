644d40ffe308d85274d6cf5fb984ba83dfb3b6e6
Peter Lyons <pete@peterlyons.com>
2011-06-22 13:57:03 -0600

    Adding tests back into prod_release

--- bin/tasks.sh
+++ bin/tasks.sh
@@ -349,7 +349,7 @@ app:prod_release() {
     eval $(ssh-agent -s) && ssh-add
     git checkout develop
     git pull origin develop
-    #BUGBUG#Disabling#jasbin || exit 5
+    app:test 
     echo "Current version is $(cat version.txt)"
     echo -n "New version: "
     read NEW_VERSION
